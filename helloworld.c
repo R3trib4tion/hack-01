e5442487b121b1c252b9a3fa4f253d4a3665c6f3
R3trib4tion <Izzatadly@gmail.com>
2019-01-17 19:05:19 -0600

    Initial commit

---
+++ helloworld.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Izzat Bin Ahmad Adly
+ * Date: 2019/01/17
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Name: Izzat Bin Ahmad Adly\n");
+  printf("Major: Mathematics\n");
+  
+  return 0;
+}
---
+++ readme.md
@@ -0,0 +1,7 @@
+# Computer Science I
+## Hack 01 - Intro to Git & Github
+
+Github URL:  
+
+Author: Izzat Bin Ahmad Adly
+Collaborators: 
